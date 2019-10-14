e55e21f9f38f8c22e4a8b763708c7c76135d8163
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-10-14 08:57:47 -0600

    setup, routes, views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>About Us</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam, nemo sit sint quis ipsa neque nobis praesentium facilis tempore aspernatur repellendus, iste velit, quas quae aut repudiandae omnis vitae.</p>
+
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// first arg corresponds with the URL, 2nd arg corresponds with the blade
+Route::view('contact', 'contact');
+// Route::get('contact', function () {
+//     return view("contact");
+// });
+
+Route::view('about', 'about');
+// Route::get('about', function () {
+//     return view('about');
+// });
